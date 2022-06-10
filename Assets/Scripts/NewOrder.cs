9cb5ab6dcf692074e5767da899a104f1ba25ae0e
Daren Liu <dliu3@scu.edu>
2022-06-10 00:37:05 -0700

    added neworder script

--- Assets/Scripts/NewOrder.cs
+++ Assets/Scripts/NewOrder.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class NewOrder : MonoBehaviour
 {
     public Text Textfield;
 
-    public void SetTest(string text)
+    public void SetTest()
     {
+        string text = "";
+        string[] ingredients = { "Corn Flakes", "Watermelon", "Apple", "Lemon", "Sauce", "Milk" };
+        var rnd = new System.Random();
+        for (int i = 0; i < 6; i++)
+        {
+            if (rnd.Next(1) == 1)
+            {
+                text += ingredients[i] + ", ";
+            }
+        }
         Textfield.text = text;
     }
 }
